29c5e564d86acce1ee546e0aab348275aa3a63f1
renbo <mychocolateisus@gmail.com>
2017-03-21 17:58:46 +0800

    update

--- ProtocolBufferDemo/app/src/main/java/com/hypers/www/protocolbufferdemo/MainActivity.java
+++ ProtocolBufferDemo/app/src/main/java/com/hypers/www/protocolbufferdemo/MainActivity.java
@@ -7,13 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,18 +20,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Log.d("", "");
-        try {
-            JSONObject options = new JSONObject();
-            JSONObject info = new JSONObject();
-            info.put("v_id", 123456789);
-            options.put("info", info);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-
         setContentView(R.layout.activity_main);
         mBtnBuild = (Button) findViewById(R.id.btn_build);
         mBtnBuild.setOnClickListener(new View.OnClickListener() {
@@ -45,12 +31,61 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void buildMrb() {
-        Addressbook.SearchRequest request =
-                Addressbook.SearchRequest.newBuilder().setQuery("apple")
-                        .setPageNumber(2)
-                        .setResultPerPage(3).build();
+//        Addressbook.SearchRequest request =
+//                Addressbook.SearchRequest.newBuilder().setQuery("apple")
+//                        .setPageNumber(2)
+//                        .setResultPerPage(3).build();
+
+        AddressBookProtos.AddressBook book =
+                AddressBookProtos.AddressBook.newBuilder()
+                        .addPeople(AddressBookProtos.Person.newBuilder()
+                                .setName("mrb")
+                                .setEmail("mrb@gmail.com")
+                                .setId(0)
+                                .addPhones(AddressBookProtos.Person.PhoneNumber.newBuilder()
+                                        .setNumber("110")
+                                        .setType(AddressBookProtos.Person.PhoneType.HOME)
+                                        .build())
+                                .build()
+                        )
+                        .addPeople(AddressBookProtos.Person.newBuilder()
+                                .setName("cyq")
+                                .setEmail("cyq@gmail.com")
+                                .setId(1)
+                                .addPhones(AddressBookProtos.Person.PhoneNumber.newBuilder()
+                                        .setNumber("112")
+                                        .setType(AddressBookProtos.Person.PhoneType.HOME)
+                                        .build())
+                                .build()
+                        )
+                        .build();
+
+        List<AddressBookProtos.Person> list = book.getPeopleList();
+        for (AddressBookProtos.Person person : list) {
+            Log.d("MainActivity", "person.getId():" + person.getId());
+            Log.d("MainActivity", "person.getName():" + person.getName());
+        }
+
+
+//        book.toBuilder().addPeople(AddressBookProtos.Person.newBuilder()
+//                .setName("cyq")
+//                .setEmail("cyq@gmail.com")
+//                .setId(1)
+//                .addPhones(AddressBookProtos.Person.PhoneNumber.newBuilder()
+//                        .setNumber("120")
+//                        .setType(AddressBookProtos.Person.PhoneType.HOME)
+//                        .build())
+//                .build())
+//                .build();
+
+//        AddressBookProtos.AddressBook.parseFrom(byte);
+
+
         File dir = Environment.getExternalStorageDirectory();
         File file = new File(dir, "address");
+        if (file.exists()) {
+            file.delete();
+        }
         if (!file.exists()) {
             try {
                 file.createNewFile();
@@ -60,9 +95,11 @@ public class MainActivity extends AppCompatActivity {
         }
         Log.d("MainActivity", file.getAbsolutePath());
         try {
-            FileOutputStream os = new FileOutputStream(file);
-            request.writeTo(os);
-            os.close();
+            byte[] arry = book.toByteArray();
+            Log.d("MainActivity", "arry.length:" + arry.length);
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(arry);
+            fos.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
